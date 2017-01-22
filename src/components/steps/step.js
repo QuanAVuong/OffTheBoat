a834eea9ea6ecf43f29802a919d3e58f26acef67
QuanAVuong <quan.a.vuong@gmail.com>
2017-01-22 16:20:05 -0500

    maps-react example

--- src/components/steps/step.js
+++ src/components/steps/step.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import simple_map_page.jsx from '../Maps.js';
 
 const Step = React.createClass({
   render() {
@@ -7,6 +8,7 @@ const Step = React.createClass({
         <div className="collapsible-header"><i className="material-icons">filter_drama</i>{this.props.text}</div>
         <div className="collapsible-body">
           {/*Google map will live here!!! */}
+          <simple_map_page.jsx />
           <p>Lorem ipsum dolor sit amet.</p>
         </div>
       </li>
